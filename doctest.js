abf311f1408330db2ec0762f8b12190a460acc84
Nikhil Patni <patninikhil047@gmail.com>
2018-02-14 04:17:16 +0530

    added evaluation criteria and create json file

---
+++ doctest.js
@@ -0,0 +1,245 @@
+const fs = require('fs');
+const natural = require('natural');
+const path = require('path');
+const textract = require('textract');
+const stringSimilarity = require('string-similarity');
+const WordPOS = require('wordpos'),
+    wordpos = new WordPOS();
+var tokenizer = new natural.WordTokenizer();
+
+var srcText,tarText;
+var src_wordcount=0,tar_wordcount=0;
+var src_noun=0,tar_noun=0;
+var src_verb=0,tar_verb=0;
+var src_adj=0,tar_adj=0;
+var status = 0;
+var tokenText;
+
+var src_address = 'source.docx';
+var tar_address = 'target.docx';
+
+var srcFile = {
+  name:"",
+  wordCount: 0,
+  nounCount: 0,
+  verbCount: 0,
+  adjCount: 0,
+};
+
+var tarFile = {
+  name:"",
+  wordCount: 0,
+  nounCount: 0,
+  verbCount: 0,
+  adjCount: 0,
+};
+
+var result = {
+  status: "",
+  score: 0,
+  similarity: 0,
+  remarks: "",
+};
+
+var data={srcFile, tarFile, result};
+
+function start(){
+  fileReadPromise(src_address,"src").then((message) => {
+    console.log(message);
+    completed();
+  })
+  .catch((message) => {
+    console.log(message);
+  });
+
+  fileReadPromise(tar_address,"target").then((message) => {
+    console.log(message);
+    completed();
+  })
+  .catch((message) => {
+    console.log(message);
+  });
+}
+
+function completed() {
+  status++;
+  if(status === 2)
+    finish();
+}
+
+function fileReadPromise(address, document) {
+  var Count = 0;
+
+  return new Promise(function(resolve,reject){
+    textract.fromFileWithPath(address, {preserveLineBreaks:true},function( error, text ) {
+      function check(){
+        Count++;
+        if(Count % 3 === 0)
+          resolve("Reading file: "+fname);
+      }
+
+      var fname = path.basename(address);
+      if(document === "src")
+        srcFile.name = fname;
+      else if(document == "target")
+        tarFile.name = fname;
+
+      if(error) {
+          reject("Document" +fname +"can not be evaluated.");
+      } else{
+      	tokenText = tokenizer.tokenize(text);
+
+        // save text for global function use
+        if(document === "src")
+        {
+        	srcText = text;
+            srcFile.wordCount = tokenText.length;
+            wordpos.getNouns(srcText,function(result){
+            	//console.log("\nNouns used: "+result);
+            	srcFile.nounCount = result.length;
+            	check();
+            });
+            wordpos.getAdjectives(srcText, function(result){
+            	srcFile.adjCount = result.length;
+            	check();
+            });
+            wordpos.getVerbs(srcText, function(result){
+            	srcFile.verbCount = result.length;
+            	check();
+            });
+        }
+        else if(document === "target")
+        {
+        	tarText = text;
+            tarFile.wordCount = tokenText.length;
+            wordpos.getNouns(tarText,function(result){
+            	tarFile.nounCount = result.length;
+            	check();
+            });
+            wordpos.getAdjectives(tarText, function(result){
+            	tarFile.adjCount = result.length;
+            	check();
+            });
+            wordpos.getVerbs(tarText, function(result){
+            	tarFile.verbCount = result.length;
+            	check();
+            });
+        }
+      }
+        
+    })
+  })
+}
+
+var finish = function(){
+	console.log(srcFile);
+	console.log(tarFile);
+
+	if(tarFile.wordCount>=srcFile.wordCount-500 && tarFile.wordCount<=srcFile.wordCount+500){
+
+		result.status = "Document Accepted on Wordcount basis";
+		var marks = 100;
+		marks -= Math.abs((srcFile.nounCount/srcFile.wordCount)*500 - (tarFile.nounCount/tarFile.wordCount)*500);
+		marks -= Math.abs((srcFile.adjCount/srcFile.wordCount)*500 - (tarFile.adjCount/tarFile.wordCount)*500);
+		marks -= Math.abs((srcFile.verbCount/srcFile.wordCount)*500 - (tarFile.verbCount/tarFile.wordCount)*500);
+
+		marks = Math.round(marks);
+
+		var similarity = stringSimilarity.compareTwoStrings(srcText, tarText);
+		result.similarity = Math.round(similarity*100);
+
+		result.score = (marks+result.similarity)/2;
+		
+		if(result.score>=80)
+			result.remarks = "Good Document. Very well Written";
+		else if(result.score>=60 && result.score<80)
+			result.remarks = "Average Document. Need more improvement";
+		else
+			result.remarks = "Poor Document";
+
+		console.log(result);
+
+	}
+
+	else{
+		result.status = "Document Rejected on Wordcount basis";
+		result.remarks = "Try to make your document under acceptable limits : [" +srcFile.wordCount-500 +"-" +srcFile.wordCount+500 +"]";
+	}
+
+	let json = JSON.stringify(data,null,2);
+	fs.writeFile('output.json',json,'utf8',(err) => {
+	  if(err){
+	    console.log("Error");
+	    return;
+	  }
+	  console.log("Success!");
+	})
+}
+
+start();
+
+/*
+var temp = path.basename(tar_address);
+var tarf_name = temp.split('.');
+var tarf_ext = path.extname(tar_address);
+console.log("Filename : "+tarf_name[0]);
+console.log("FileExtension : "+tarf_ext);
+
+var twoinone = function(file,key){
+	return new Promise((resolve,reject)=>{
+		//reading files
+		textract.fromFileWithPath(src_address,{preserveLineBreaks:true}, function( error, text ) {
+			if(error)
+				console.log("Error Code : "+error);
+			else{
+				//console.log(text);
+				srcFile = text;
+				tokenText = tokenizer.tokenize(srcFile);
+				src_wordcount = tokenText.length;
+				console.log("Source file Wordcount : " +src_wordcount);
+				wordpos.getNouns(srcFile, function(result){
+					src_noun = (result.length/src_word) * 500;
+					console.log(src_noun);
+				});
+				wordpos.getVerbs(srcFile, function(result){
+		    		src_verb = (result.length/src_word) * 500;
+		    		console.log(src_verb);
+				});
+				wordpos.getAdjectives(srcFile, function(result){
+		    		src_adj = (result.length/src_word) * 500;
+		    		console.log(src_adj);
+				});
+			}
+
+		})
+	});
+};
+
+let promises = [];
+let f1 = twoinone()
+
+textract.fromFileWithPath(tar_address,{preserveLineBreaks:true}, function( error, text ) {
+	if(error)
+		console.log("Error! The file " +tarf_name+tarf_ext +"can not be evaluated");
+	else{
+		//console.log(text);
+		tarFile = text;
+		tokenText = tokenizer.tokenize(tarFile);
+		tar_wordcount = tokenText.length;
+		console.log("Target file Wordcount : " +tar_wordcount);
+		wordpos.getNouns(tarFile, function(result){
+			tar_noun = (result.length/tar_word) * 500;
+			console.log(tar_noun);
+		});
+		wordpos.getVerbs(tarFile, function(result){
+    		tar_verb = (result.length/tar_word) * 500;
+    		console.log(tar_verb);
+		});
+		wordpos.getAdjectives(tarFile, function(result){
+    		tar_adj = (result.length/tar_word) * 500;
+    		console.log(tar_adj);
+		});
+	}
+
+})
+*/
---
+++ js/main.js
@@ -0,0 +1,35 @@
+function getdata(){
+  var xhttp = new XMLHttpRequest();
+  var url="http:/localhost:3000/db";
+
+  xhttp.open("GET",url, true);
+  xhttp.send();
+	
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myArr=JSON.parse(this.responseText);
+      //var dataObj=JSON.stringify(myArr,null,20);
+      var show=document.getElementById('msg1');
+      show.innerHTML="<table width='500' border='1'><tr><th>Parameter</th><th>Source Document</th><th>Your Document</th></tr>"
+      +"<tr><td><strong>File Name</strong></td><td>"+myArr.srcFile.name+"</td><td>"+myArr.tarFile.name+"</td></tr>"
+      +"<tr><td><strong>Word Count</strong></td><td>"+myArr.srcFile.wordCount+"</td><td>"+myArr.tarFile.wordCount+"</td></tr>"
+      +"<tr><td><strong>Noun Count</strong></td><td>"+myArr.srcFile.nounCount+"</td><td>"+myArr.tarFile.nounCount+"</td></tr>"
+      +"<tr><td><strong>Verb Count</strong></td><td>"+myArr.srcFile.verbCount+"</td><td>"+myArr.tarFile.verbCount+"</td></tr>"
+      +"<tr><td><strong>Adjective Count</strong></td><td>"+myArr.srcFile.adjCount+"</td><td>"+myArr.tarFile.adjCount+"</td></tr></table>"
+
+      /*"<h2>Source Document </h2>"+"File name : "+"<b>"+myArr.srcFile.name+"</b>"+
+      "</br>Word Count : "+"<b>"+myArr.srcFile.wordCount+"</b>"+"</br>Noun Count : "+"<b>"+
+      myArr.srcFile.nounCount+"</b>"+"</br>Verb Count : "+"<b>"+myArr.srcFile.verbCount+"</b>"+"</br>Adjective Count : "+"<b>"+myArr.srcFile.adjCount+"</b>"+
+      
+      "<h2>Your Document </h2>"+"File name : "+"<b>"+myArr.tarFile.name+"</b>"+
+      "</br>Word Count : "+"<b>"+myArr.tarFile.wordCount+"</b>"+"</br>Noun Count : "+"<b>"+
+      myArr.tarFile.nounCount+"</b>"+"</br>Verb Count : "+"<b>"+myArr.tarFile.verbCount+"</b>"+"</br>Adjective Count : "+"<b>"+myArr.tarFile.adjCount*/
+
+      +"</b>"+
+
+      "<h3>Final Result </h3>"+"Status : "+"<b>"+myArr.result.status+"</b>"+
+      "</br>Similarity from Source Document : "+"<b>"+myArr.result.similarity+"%"+"</b>"+"</br>Score (Out of 100) : "+"<b>"+
+      myArr.result.score+"</b>"+"</br>Remarks : "+"<b>"+myArr.result.remarks+"</b>";
+    }
+  };
+}
