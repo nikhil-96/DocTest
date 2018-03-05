f62c0848fc293424336aac8d55373174ca53db3a
Nikhil Patni <patninikhil047@gmail.com>
2018-03-05 17:24:56 +0530

    removing redundancy

--- doctest.js
+++ doctest.js
@@ -7,15 +7,17 @@ var tokenizer = new natural.WordTokenizer();
 var base_folder = path.join(path.dirname(require.resolve("natural")), "brill_pos_tagger");
 var rulesFilename = base_folder + "/data/English/tr_from_posjs.txt";
 var lexiconFilename = base_folder + "/data/English/lexicon_from_posjs.json";
-var defaultCategory = 'N';
+var defaultCategory = '';
 
 var lexicon = new natural.Lexicon(lexiconFilename, defaultCategory);
 var rules = new natural.RuleSet(rulesFilename);
 var tagger = new natural.BrillPOSTagger(lexicon, rules);
 
-var srcText,tarText;
+var text,srcText,tarText;
 var status = 0;
 var tokenText;
+var i=1;                                        //variable to get output from json array returned by pos-tagger
+var marks = 100,good = 80,avg = 60;             //setting up marks for evaluating the document
 
 // getting address of both files
 var src_address = 'res/source.txt';
@@ -77,58 +79,76 @@ function ReadPromise(address, document) {
   var Count = 0;
 
   return new Promise(function(resolve,reject){
-    var text = fs.readFileSync(address,'utf8');
+    text = fs.readFileSync(address,'utf8');
       function check(){
           resolve("Reading file: "+fname);
       }
 
       var fname = path.basename(address);     //extracting file name
-      if(document === "src")
-        srcFile.name = fname;
-      else if(document === "target")
-        tarFile.name = fname;
-
       if(text === null) {
-          reject("Document" +fname +"can not be evaluated.");
+          reject("File" +fname +"corrupted.");
       } else{
-      	tokenText = tokenizer.tokenize(text);          //file converted into array of words
-
-        var taggerJSON = tagger.tag(tokenText);
-
-        if(document === "src")
-        {     
-          // srcText is for use in string-similarity
-        	srcText = text;
+        tokenText = tokenizer.tokenize(text);          //file converted into array of words
+        if(document === "src"){
+            srcFile.name = fname;
+            srcText = text;                             // srcText is for use in string-similarity
             srcFile.wordCount = tokenText.length;       // counting no of words in source file
-             for(var arr of taggerJSON){
-                  if(arr[1] == "NN" || arr[1] == "NNP" || arr[1] == "NNPS")
-                    srcFile.nounCount++;
-                  else if(arr[1] == "VB" || arr[1] == "VBD" || arr[1] == "VBG" || arr[1] == "VBN" || arr[1] == "VBP" || arr[1] == "VBZ")
-                    srcFile.verbCount++;
-                  else if(arr[1] == "JJ" || arr[1] == "JJR" || arr[1] == "JJS")
-                    srcFile.adjCount++;
-                
-                }
-          check();
+            filePOS("src");
+            check();
         }
-        else if(document === "target")
-        {
-        	tarText = text;
+        
+      else if(document === "target"){
+            tarFile.name = fname;
+            tarText = text;
             tarFile.wordCount = tokenText.length;       // counting no of words in target file
-            for(var arr of taggerJSON){
-                  if(arr[1] == "NN" || arr[1] == "NNP" || arr[1] == "NNPS")
-                    tarFile.nounCount++;
-                  else if(arr[1] == "VB" || arr[1] == "VBD" || arr[1] == "VBG" || arr[1] == "VBN" || arr[1] == "VBP" || arr[1] == "VBZ")
-                    tarFile.verbCount++;
-                  else if(arr[1] == "JJ" || arr[1] == "JJR" || arr[1] == "JJS")
-                    tarFile.adjCount++;
-                
-                }
-          check();
-        }
+            filePOS("target");
+            check();
       }
+    }
         
   })
+}
+
+function filePOS(doc) {
+    var taggerJSON = tagger.tag(tokenText);
+
+    for(var arr of taggerJSON){
+          if(arr[i] == "NN" || arr[i] == "NNP" || arr[i] == "NNPS")
+          {
+            if(doc === "src")
+            {
+                srcFile.nounCount++;
+            }
+            else if(doc === "target")
+            {
+                tarFile.nounCount++;
+            }
+            
+          }
+          else if(arr[i] == "VB" || arr[i] == "VBD" || arr[i] == "VBG" || arr[i] == "VBN" || arr[i] == "VBP" || arr[i] == "VBZ")
+           { if(doc === "src")
+            {
+                srcFile.verbCount++;
+            }
+            else if(doc === "target")
+            {
+                tarFile.verbCount++;
+            }
+            
+          }
+          else if(arr[i] == "JJ" || arr[i] == "JJR" || arr[i] == "JJS"){
+            if(doc === "src")
+            {
+                srcFile.adjCount++;
+            }
+            else if(doc === "target")
+            {
+                tarFile.adjCount++;
+            }
+            
+          }
+        
+        }
 }
 
 var finish = function(){
@@ -138,29 +158,25 @@ var finish = function(){
 	if(tarFile.wordCount>=srcFile.wordCount-1000 && tarFile.wordCount<=srcFile.wordCount+1000){    // Wordcount criteria
 
 		result.status = "Document Accepted on Wordcount basis";
-		var marks = 100;
+		
 
     // reducing marks on the basis of difference from source file
 		marks -= Math.abs((srcFile.nounCount/srcFile.wordCount)*500 - (tarFile.nounCount/tarFile.wordCount)*500);
 		marks -= Math.abs((srcFile.adjCount/srcFile.wordCount)*500 - (tarFile.adjCount/tarFile.wordCount)*500);
 		marks -= Math.abs((srcFile.verbCount/srcFile.wordCount)*500 - (tarFile.verbCount/tarFile.wordCount)*500);
-
-		marks = Math.round(marks);        //Rounding off marks 
-
-		var similarity = natural.JaroWinklerDistance(srcText,tarText);   //calculating similarity between source and target files
+    marks = Math.round(marks);        //Rounding off marks 
+    var similarity = natural.JaroWinklerDistance(srcText,tarText);   //calculating similarity between source and target files
 		result.similarity = Math.round(similarity*100);     //rounding off similarity
 
 		result.score = (marks+result.similarity)/2;
-		
-		if(result.score>=80)                  //remarks criteria
+		if(result.score>=good)                  //remarks criteria
 			result.remarks = "Good Document. Very well Written";
-		else if(result.score>=60 && result.score<80)
+		else if(result.score>=avg && result.score<good)
 			result.remarks = "Average Document. Need more improvement";
 		else
 			result.remarks = "Poor Document";
 
 		console.log(result);
-
 	}
 
 	else{
